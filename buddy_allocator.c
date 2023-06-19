62631f0868d57c9c933e6042c4e3f42a84098d13
paolocupini <cupini.1941511@studenti.uniroma1.it>
2023-06-19 18:21:36 +0200

    added files + prof's code

---
+++ buddy_allocator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <assert.h>
+#include <math.h> // for floor and log2
+#include "buddy_allocator.h"
+
+// these are trivial helpers to support you in case you want
+// to do a bitmap implementation
+//! copy and paste from prof's code
+
+//level of node idx
+int levelIdx(size_t idx){
+  return (int)floor(log2(idx));
+};
+//index of the buddy with idx 
+int buddyIdx(int idx){
+  if (idx&0x1){
+    return idx-1;
+  }
+  return idx+1;
+}
+
+int parentIdx(int idx){
+  return idx/2;
+}
+
+int startIdx(int idx){
+  return (idx-(1<<levelIdx(idx)));
+}
